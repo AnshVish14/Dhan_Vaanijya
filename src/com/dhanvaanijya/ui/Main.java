f34a68c428b10a8bb8987b4b3fa655866323b06a
AnshVish14 <supriyatamrakar374@gmail.com>
2025-05-29 15:03:19 +0530

    The new file structure

--- src/com/dhanvaanijya/ui/Main.java
+++ src/com/dhanvaanijya/ui/Main.java
@@ -7,12 +7,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("WELCOME TO DHANVAANIJYA");
         System.out.println("'' HAR HAR MAHADEV ' Starting DHAN VAANIJYA...");
-             try {
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             UIManager.put("MenuBar.background", Color.LIGHT_GRAY);
         } catch (Exception e) {
             e.printStackTrace();
-        }        // Open the Dashboard
+        }
+
+        // Launch GUI
         javax.swing.SwingUtilities.invokeLater(() -> {
             new Dashboard().setVisible(true);
         });
